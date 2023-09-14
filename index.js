65a0b6199671127c84b234f472822ba0ef217824
YS-tool <yanlinchristopher@gmail.com>
2023-09-14 08:39:41 -0700

    add mongoose, cron, create mongodb connection

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express();
 const session = require('express-session');
 const flash = require('connect-flash');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const MONGOuri = "mongodb+srv://YS_self:9QSpVoQcduhVPaVl@selfwebsite.dqqhad2.mongodb.net/";
 
 app.set('views', __dirname+'/views');
 app.set('view engine', 'ejs');
@@ -24,8 +26,26 @@ app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 
+mongoose.connect(MONGOuri,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    retryWrites:true,
+    w:"majority"
+  }
+);
+
+// routes
 app.use('/', require("./routes/index"));
 
+
+// var task = require("./config/cronjob")
+
+// task.start()
+
+
+
+// start server
 if(config.inRuntime()){
   // Get PORT and start the server
   app.listen(config.port, function() {
@@ -37,5 +57,27 @@ if(config.inRuntime()){
   });
 }
 
+//-----------------------------------------------------------------------------------
+// outline for this branch
+
+// 1: add mongo connector
+// 2: add mongoose model
+// 3: For example: when go to one page, write to database one record
+
+// 4: have a user schema. For example when user click on one page, auto create one page. 
+// 5: if possible, have a cron job clean the user collection
 
 
+//-----------------------------------------------------------------------
+// next branch
+// UI for login, Add MFA is possible
+
+// 1: create login UI and backend for user login
+// 2: check cisco MFA or other MFA. Or google authenticator
+
+//  "Remember this device" feature
+
+// have a collection for user to play around
+
+
+// use cookie
